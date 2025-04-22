07655a87e705e8d42f49ec91ee24d6f3a91b3117
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-04-22 10:52:38 +0200

    Added Error Middleware

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, resp) => {
   resp.send("Welcome to the Subscription Tracker");
